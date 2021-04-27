51ae04dce84825315505d5c414633ebf0cd0a86a
Stefan Z <stefansteff999@gmail.com>
2021-04-27 14:57:56 +0300

    revert

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,6 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {secure: true},
 }))
 app.use(passport.initialize())
 app.use(passport.session())
